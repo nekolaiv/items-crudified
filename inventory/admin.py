f6dc1ae47c931ac7e65493892bf8102da181beff
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-07 20:39:28 +0800

    update: add and register models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Item
+from .models import Food, Gadget
 
 # Register your models here.
 
-admin.site.register(Item)
+# admin.site.register(Item)
+admin.site.register(Food)
+admin.site.register(Gadget)
--- inventory/models.py
+++ inventory/models.py
@@ -2,11 +2,33 @@ from django.db import models
 
 # Create your models here.
 
-class Item(models.Model):
+# class Item(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.name
+    
+
+class Food(models.Model):
+    id=models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=100)
-    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Gadget(models.Model):
+    id=models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
