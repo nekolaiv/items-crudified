9c60531c9998b39431b4dc081ab276d96ad5bb84
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-10 08:19:28 +0800

    update: setup necessary parameters, delete id attribute

--- inventory/models.py
+++ inventory/models.py
@@ -2,22 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-# class Item(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.name
-    
-
 class Food(models.Model):
-    id=models.IntegerField(primary_key=True, auto_created=True)
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -25,10 +13,9 @@ class Food(models.Model):
     
 
 class Gadget(models.Model):
-    id=models.IntegerField(primary_key=True, auto_created=True)
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
