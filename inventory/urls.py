3730e9f2d5c134f3371b9636d1220387a473cc21
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-11 14:52:57 +0800

    update: remove gadget urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import FoodCreateView, FoodUpdateView, FoodDeleteView, GadgetCreateView, GadgetDeleteView, GadgetUpdateView, HomeListView
+from .views import FoodCreateView, FoodUpdateView, FoodDeleteView, HomeListView
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('food/add/', FoodCreateView.as_view(), name='add_food'),
-    path('gadget/add/', GadgetCreateView.as_view(), name='add_gadget'),
     path('food/edit/<int:pk>/', FoodUpdateView.as_view(), name='edit_food'),
-    path('gadget/edit/<int:pk>/', GadgetUpdateView.as_view(), name='edit_food'),
     path('food/delete/<int:pk>/', FoodDeleteView.as_view(), name='food_delete'),
-    path('gadget/delete/<int:pk>/',
-         GadgetDeleteView.as_view(), name='gadget_delete'),
 ]
