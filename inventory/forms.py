039bcdd8e22ceceb2291678712dc334b42f52579
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-11 09:33:30 +0800

    update: add styles for input forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -6,9 +6,29 @@ class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
         fields = ['name', 'price']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
+                'placeholder': 'Enter food name...'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
+                'placeholder': 'Enter price...'
+            }),
+        }
 
 
 class GadgetForm(forms.ModelForm):
     class Meta:
         model = Gadget
         fields = ['name', 'price']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
+                'placeholder': 'Enter gadget name...'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
+                'placeholder': 'Enter price...'
+            }),
+        }
