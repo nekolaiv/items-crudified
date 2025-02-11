a2667c83fa6515b1f357331d63bc75c6eb828dc3
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-11 13:25:24 +0800

    update: remove gadget views

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import CreateView, UpdateView, TemplateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Food, Gadget
-from .forms import FoodForm, GadgetForm
+from django.http import JsonResponse
+from .models import Food
+from .forms import FoodForm
 
 # Create your views here.
 
@@ -12,8 +13,16 @@ class HomeListView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['foods'] = Food.objects.all()
-        context['gadgets'] = Gadget.objects.all()
         return context
+
+    def get(self, request, *args, **kwargs):
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            query = request.GET.get('q', '')
+            foods = list(Food.objects.filter(
+                name__icontains=query).values('id', 'name', 'price'))
+            return JsonResponse({'foods': foods})
+
+        return super().get(request, *args, **kwargs)
 
 
 class FoodCreateView(CreateView):
@@ -32,25 +41,5 @@ class FoodUpdateView(UpdateView):
 
 class FoodDeleteView(DeleteView):
     model = Food
-    template_name = 'confirm_delete.html'
-    success_url = reverse_lazy('home')
-
-
-class GadgetCreateView(CreateView):
-    model = Gadget
-    form_class = GadgetForm
-    template_name = 'gadget_form.html'
-    success_url = reverse_lazy('home')
-
-
-class GadgetUpdateView(UpdateView):
-    model = Gadget
-    form_class = GadgetForm
-    template_name = 'gadget_form.html'
-    success_url = reverse_lazy('home')
-
-
-class GadgetDeleteView(DeleteView):
-    model = Gadget
     template_name = 'confirm_delete.html'
     success_url = reverse_lazy('home')
