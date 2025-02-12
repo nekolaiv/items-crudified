2c02f5b061a2f9683a53805ec184c94a0396fe1a
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-12 23:41:46 +0800

    update: add table form class, remove gadget form class

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Food, Gadget
+from .models import Food, Table
+
+
+class Table(forms.ModelForm):
+    class Meta:
+        model = Table
+        fields = ["name"]
 
 
 class FoodForm(forms.ModelForm):
@@ -10,22 +16,6 @@ class FoodForm(forms.ModelForm):
             'name': forms.TextInput(attrs={
                 'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
                 'placeholder': 'Enter food name...'
-            }),
-            'price': forms.NumberInput(attrs={
-                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
-                'placeholder': 'Enter price...'
-            }),
-        }
-
-
-class GadgetForm(forms.ModelForm):
-    class Meta:
-        model = Gadget
-        fields = ['name', 'price']
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
-                'placeholder': 'Enter gadget name...'
             }),
             'price': forms.NumberInput(attrs={
                 'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
