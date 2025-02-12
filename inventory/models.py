f0ea7924d8400e73b2ff09ecb98d7e162af3ad21
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-12 23:42:03 +0800

    update: remove gadget model add table model

--- inventory/models.py
+++ inventory/models.py
@@ -3,17 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class Food(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Table(models.Model):
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
 
-class Gadget(models.Model):
+class Food(models.Model):
     name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
