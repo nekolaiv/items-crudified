bcd52603bfa3826b197e77b208ff67e3bcb591db
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-14 10:00:24 +0800

    update: refactor item model

--- inventory/models.py
+++ inventory/models.py
@@ -10,11 +10,12 @@ class Table(models.Model):
         return self.name
 
 
-class Food(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Item(models.Model):
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
