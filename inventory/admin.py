28fd06ad711e82ee96156ef56315ca203be09588
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-14 09:59:03 +0800

    update: register Item Model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Food, Table
+from .models import Table, Item
 
 # Register your models here.
 
 
 admin.site.register(Table)
-admin.site.register(Food)
+admin.site.register(Item)
