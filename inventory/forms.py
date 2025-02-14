b070193badbff53c5da866e57bd3c431ca282a67
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-14 10:00:02 +0800

    update: rename food  to item, add widget for styles in table form

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,16 +1,22 @@
 from django import forms
-from .models import Food, Table
+from .models import Table, Item
 
 
-class Table(forms.ModelForm):
+class TableForm(forms.ModelForm):
     class Meta:
         model = Table
         fields = ["name"]
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full p-2 rounded-lg border border-zinc-800 bg-zinc-950 text-zinc-400',
+                'placeholder': 'Enter table name...'
+            }),
+        }
 
 
-class FoodForm(forms.ModelForm):
+class ItemForm(forms.ModelForm):
     class Meta:
-        model = Food
+        model = Item
         fields = ['name', 'price']
         widgets = {
             'name': forms.TextInput(attrs={
