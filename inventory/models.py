5a52c42f35c87eb1c4d44c8b16d3196a5cbaa155
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-14 17:19:00 +0800

    update: remove description attribute

--- inventory/models.py
+++ inventory/models.py
@@ -14,7 +14,6 @@ class Item(models.Model):
     table = models.ForeignKey(
         Table, on_delete=models.CASCADE, related_name="items")
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
