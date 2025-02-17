bdc7c8575d6a3895f94987a5c316642bdc552c77
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-02-17 10:23:34 +0800

    update: add update and delete table urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import (
-    TableAndItemView, TableCreateView, ItemCreateView, ItemUpdateView, ItemDeleteView
+    TableAndItemView, TableCreateView, ItemCreateView, ItemUpdateView, ItemDeleteView, TableDeleteView, TableUpdateView
 )
 
 urlpatterns = [
     path("", TableAndItemView.as_view(), name="table_view"),
     path("tables/new/", TableCreateView.as_view(), name="table_create"),
+    path("tables/<int:pk>/table/edit/",
+         TableUpdateView.as_view(), name="table_update"),
+    path("tables/<int:pk>/table/delete/",
+         TableDeleteView.as_view(), name="table_delete"),
     path("tables/<int:table_id>/items/add/",
          ItemCreateView.as_view(), name="item_create"),
     path("tables/<int:table_id>/items/<int:pk>/edit/",
